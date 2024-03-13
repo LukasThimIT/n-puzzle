2009946d8cf038cf80eb9f49ea968b4a257113e9
Lukas Thim <45878935+LukasThimIT@users.noreply.github.com>
2024-03-13 17:55:54 +0100

    Added squares with functions to change label

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Square({value, onSquareClick}){
+  return (<button className="square" onClick={onSquareClick}>
+              {value}
+          </button>);
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null)); 
+  
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
+  return(
+    <>
+      <div class="board-row">
+        <Square value={squares[0]} onSquareClick={0} />
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
+      </div>
+      <div class="board-row">
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
+      </div>
+      <div class="board-row">
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
+      </div>
+    </>
+  );
+  }
